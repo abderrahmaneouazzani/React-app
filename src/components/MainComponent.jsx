bfd91ee7f1ee581dc1dc1687f67a77a40e2424b9
abderrahmaneouazzani <abderrahmaneouazzani@gmail.com>
2020-07-14 21:05:32 +0100

    Configuring About Component

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -4,6 +4,7 @@ import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -51,6 +52,8 @@ class Main extends Component {
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact}  />
+              <Route path='/aboutus' component={() => <About leaders={this.state.leaders}/>} />
+              <Route exact path='/aboutus' component={About}  />
               <Redirect to="/home" />
           </Switch>
         <Footer />
